40d5c91df97b43ec1de2e79ae69f550726f7fe6f
Patricia Atanasoff <system.informatica365@gmail.com>
2026-04-29 19:48:13 -0300

    cambio de estilos y toast cuando no hay stock

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -8,9 +8,20 @@ const ItemCount = ({onAdd, max, initial }) => {
     const add = () => {
         if (value < max) {
             setValue(value + 1);
+        } else {
+            Toastify({
+                text: "No hay más stock disponible",
+                position: "center",
+                gravity: "bottom",
+                duration: 2000,
+                style: {
+                    background: "#BF0B3B",
+                    padding: "15px",
+                    fontSize: "16px"
+                }
+            }).showToast();
         }
     }
-
     const subtract = () => {
         if (value > initial) {
             setValue(value - 1)
@@ -23,10 +34,35 @@ const ItemCount = ({onAdd, max, initial }) => {
                 <div className="contador" >
                     <button className="btn-contador" onClick={subtract}>-</button>
                     <p>{value}</p>
-                    <button className="btn-contador" onClick={add}>+</button>
+                    <button 
+  className={`btn-contador ${value >= max ? "btn-disabled" : ""}`}
+  onClick={add}
+>
+  +
+</button>
                     <button className="btn-contador" onClick={() => setValue(1)}>Reset</button>
                 </div>
-                <button className="btn-contador" onClick={() => onAdd(value)} disabled={max === 0}>Añadir producto </button>
+                <button 
+  className={`btn-contador ${max === 0 ? "btn-disabled" : ""}`}
+  onClick={() => {
+    if (max === 0) {
+      Toastify({
+        text: "Producto sin stock",
+        duration: 2000,
+        gravity: "bottom",
+        position: "center",
+        style: {
+          background: "#BF0B3B",
+          padding: "15px"
+        }
+      }).showToast();
+    } else {
+      onAdd(value);
+    }
+  }}
+>
+  Añadir producto
+</button>
                 <NavLink to='/catalogue'><button className="btn-contador"> Volver al catálogo </button> </NavLink>
             </div>
         </>
